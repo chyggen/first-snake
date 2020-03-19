a0880e3ed1c75968cbf9fc24332a34cf67ff01d2
Chris Hyggen <chyggenot@gmail.com>
2020-03-19 14:36:58 -0700

    object copy fix 2

--- server.py
+++ server.py
@@ -61,17 +61,23 @@ class Battlesnake(object):
         def print_board(board):
             for y in range(len(board[0])):
                 for x in range(len(board)):
-                    print(board[x][y], end = " ")
+                    if (board[x][y] == "empty"):
+                        print("-----", end = " ")
+                    else: print(board[x][y], end = " ")
                     if (x == len(board)-1):
                         print("")
 
+        def print_snake(snake):
+            for i in range(len(snake.body)):
+                print(f"x : {snake.body[i].x}, y : {snake.body[i].y} ")
 
         def update_board(board, snakes):
 
             # print("board before update:")
             # print_board(board)
             #clear board
-            board = [["empty" for i in range(0, data.get("board").get("width"))] for j in range(0, data.get("board").get("height"))]
+            board_copy = board
+            board_copy = [["empty" for i in range(0, data.get("board").get("width"))] for j in range(0, data.get("board").get("height"))]
             
             #Flag board indicies that are occupied by a snake to "head" or "body"
             for s in range(0, len(snakes)): #For every snake
@@ -82,59 +88,51 @@ class Battlesnake(object):
                     # print("s,p")
                     # print(s)
                     # print(p)
-                    if (board[snakes[s].body[p].x][snakes[s].body[p].y] == "empty"): 
+                    if (board_copy[snakes[s].body[p].x][snakes[s].body[p].y] == "empty"): 
                         if (p == 0):
-                            board[snakes[s].body[p].x][snakes[s].body[p].y] = "head" 
-                        else: board[snakes[s].body[p].x][snakes[s].body[p].y] = "body"
+                            board_copy[snakes[s].body[p].x][snakes[s].body[p].y] = "head " 
+                        else: board_copy[snakes[s].body[p].x][snakes[s].body[p].y] = "body "
             
             # print("board after update:")
             # print_board(board)
 
-            return board
-
-
+            return board_copy
 
         def check_moves(moves, board, mysnake, snakes):
 
-            print("moves at start:")
-            print(moves)
 
-            if (mysnake.head.x == 0 and "left" in moves):
+            if (snakes[0].body[0].x == 0 and "left" in moves):
                 moves.remove('left')
-            elif (mysnake.head.x == boardsize.x - 1 and "right" in moves):
+            elif (snakes[0].body[0].x == boardsize.x - 1 and "right" in moves):
                 moves.remove('right')
 
-            if (mysnake.head.y == 0 and "up" in moves):
+            if (snakes[0].body[0].y == 0 and "up" in moves):
                 moves.remove('up')
-            elif (mysnake.head.y == boardsize.y - 1 and "down" in moves):
+            elif (snakes[0].body[0].y == boardsize.y - 1 and "down" in moves):
                 moves.remove('down')
 
 
-            print("moves after wall collision:")
-            print(moves)
-            
             #Remove possibilty of hitting a snake
             if "left" in moves:
-                if (board[snakes[0].head.x - 1][snakes[0].head.y] != "empty" ):
+                if (board[snakes[0].body[0].x - 1][snakes[0].body[0].y] != "empty" ):
                     moves.remove('left')
 
             if "right" in moves:
-                if (board[snakes[0].head.x + 1][snakes[0].head.y] != "empty" ):
+                if (board[snakes[0].body[0].x + 1][snakes[0].body[0].y] != "empty" ):
                     moves.remove('right')
 
             if "up" in moves:
-                if (board[snakes[0].head.x][snakes[0].head.y - 1] != "empty" ):
+                if (board[snakes[0].body[0].x][snakes[0].body[0].y - 1] != "empty" ):
                     moves.remove('up')       
 
             if "down" in moves:
-                if (board[snakes[0].head.x][snakes[0].head.y + 1] != "empty" ):
+                if (board[snakes[0].body[0].x][snakes[0].body[0].y + 1] != "empty" ):
                     moves.remove('down')     
             
-            print("moves at end:")
-            print (moves)
 
         def simulate_move(move, board, mysnake, snakes):
 
+            print(f"simulating {move}")
             board_copy = board
             snakes_copy = snakes
             mysnake_copy = mysnake
@@ -142,11 +140,9 @@ class Battlesnake(object):
 
             for s in range(0, len(snakes_copy)):
                 #For each body part 
-                for p in range(len(snakes_copy[s].body) - 1, 0, -1):
+                for p in range(len(snakes_copy[s].body) -1, 0, -1):
                     #Shift one turn forward
-                    snakes_copy[s].body[p] = snakes_copy[s].body[p-1]
-                #Delete tail
-                #del (snakes_copy[s].body[len(snakes_copy[s].body) - 1])
+                    snakes_copy[s].body[p] = coord(snakes_copy[s].body[p-1].x, snakes_copy[s].body[p-1].y)
 
             #For my snake
 
@@ -165,28 +161,27 @@ class Battlesnake(object):
             if (move == "down"):
                 snakes_copy[0].head.y += 1 
                 next_moves.remove("up")
-            
-            snakes_copy[0].body[0] = snakes_copy[0].head
 
-            mysnake = snakes_copy[0]
+            snakes_copy[0].body[0] = snakes_copy[0].head 
+        
+            mysnake_copy = snakes_copy[0]
 
             board_copy = update_board(board_copy, snakes_copy)
 
-            print_board(board_copy)
-
             check_moves(next_moves, board_copy, mysnake_copy, snakes_copy)
 
-            print(f"move: {move}")
             print(f"next moves: {next_moves}")
 
-
             if (len(next_moves) == 1):
+                print(f"move {move} inconclusive, recurring")
                 return simulate_move(next_moves[0], board_copy, mysnake_copy, snakes_copy)
             elif (len(next_moves) == 0):
+                print(f"move {move} not valid, exiting")
                 return False
-            else: return True
-
-
+            else: 
+                print(f"move {move} valid, exiting")
+                return True
+                
 
 
         #Interepert game data
@@ -199,32 +194,35 @@ class Battlesnake(object):
         boardsize = coord(data.get("board").get("width"), data.get("board").get("height"))
 
 
+
         #Flag board indicies that are occupied by a snake to "head" or "body"
         for s in range(0, len(snakes)): #For every snake
             for p in range(0, len(snakes[s].body)): #For each body part of snake "s"
                 if (board[snakes[s].body[p].x][snakes[s].body[p].y] == "empty"): 
                     if (p == 0):
-                        board[snakes[s].body[p].x][snakes[s].body[p].y] = "head" 
-                    else: board[snakes[s].body[p].x][snakes[s].body[p].y] = "body"
+                        board[snakes[s].body[p].x][snakes[s].body[p].y] = "head " 
+                    else: board[snakes[s].body[p].x][snakes[s].body[p].y] = "body "
 
         #TODO Flag board indicies that contain food
 
         #Contains possible moves
-        possible_moves = ["up", "down", "left", "right"]
+
+        print("original board:")
+        print_board(board)
+        possible_moves = ["up", "down", "right", "left"]
 
         check_moves(possible_moves, board, mysnake, snakes)
 
-        print (f"possible moves: {possible_moves}")
-
+        print (f"possible moves at start: {possible_moves}")
 
         for i in range(len(possible_moves)-1, -1 , -1):
-            if simulate_move(possible_moves[i], board, mysnake, snakes) == False:
+            if simulate_move(possible_moves[i], board, mysnake, [snake(num, len(data.get("board").get("snakes")[num].get("body"))) for num in range(0, len(data.get("board").get("snakes")))]) == False:
                 possible_moves[i].remove
 
-        print(f"possible moves:{possible_moves}")
+        print(f"possible moves after recursion:{possible_moves}")
 
         move = random.choice(possible_moves)
-        print(f"MOVE: {move}")
+        print(f"random move: {move}")
         return {"move": move}
 
     @cherrypy.expose
